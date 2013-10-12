74c73c666ceb0d84616ce52edef581e923d4c6f6
Erik S. Carlsten <ecarlste.register@gmail.com>
2013-10-12 10:10:11 -0500

    Added skeleton code for plugin
    - Added wp-content/plugins/greeting-card-gallery directory
    - Added greeting-card-gallery.php
    - Inserted header information into greeting-card-gallery.php

---
+++ wp-content/plugins/greeting-card-gallery/greeting-card-gallery.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Greeting Card Gallery
+ * Plugin URI: http://www.bigskyreflections.com
+ * Description: A Greeting Card Gallery Plugin.
+ * Version: 0.1
+ * Author: Erik S. Carlsten
+ * Author URI: http://www.bigskyreflections.com
+ * License: GPL2
+ */
+?>
